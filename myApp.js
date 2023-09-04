212aab100d4960aa9267944f01366ef0b48e8ec5
Kalebhe2222 <143212208+Kalebhe2222@users.noreply.github.com>
2023-09-04 01:02:50 -0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -16,7 +16,7 @@ app.get('/',function(req,res){
   app.use('/public', express.static(__dirname +   "/public"));
   app.get("/json",function(req,res){
  process.env.MESSAGE_STYLE;
-    if(process.env.MESSAGE_STYLE == "uppercase"){
+    if(process.env.VAR_MESSAGE_STYLE == "uppercase"){
        res.json({"message": "HELLO JSON"});
     }else{
            res.json({"message": "Hello json"});
