cb5a683c5968f5b435416af6d44edcc6e10966cf
Kalebhe Gualter <143212208+Kalebhe2222@users.noreply.github.com>
2023-09-06 01:12:46 -0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,31 +1,32 @@
 let express = require('express');
 let app = express();
+let dotenv = require('dotenv').config();
 
 
+app.get('/', function(req, res) {
 
-app.get('/',function(req,res){
 
-  
 
   res.sendFile(__dirname + "/views/index.html");
+
+})
+
+
+
+app.use('/public', express.static(__dirname + "/public"));
+app.get("/json", function(req, res) {
   
-  })
-
-
-   
-  app.use('/public', express.static(__dirname +   "/public"));
-  app.get("/json",function(req,res){
-
-   const env = app.use(express.static(__dirname +   "process.env.MESSAGE_STYLE"));
-    if ( env === 'uppercase') {
-  res.json({"message": "HELLO JSON"});
-} else {
-  res.json({"message": "Hello json"});
+var  message = process.env['VAR_MESSAGE_STYLE']
+if (   message == "uppercase"){
+    res.json = ({"message": "HELLO JSON"});
+  
+}else{
+  res.json = ({"message": "Hello json"});
 }
-    
-  })
   
 
+})
+module.exports =app;
 
 
 
@@ -58,4 +59,15 @@ app.get('/',function(req,res){
 
 
 
- module.exports = app;
+
+
+
+
+
+
+
+
+
+
+
+module.exports = app;
